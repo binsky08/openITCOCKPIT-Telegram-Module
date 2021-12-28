b8f3ebdabd2f6c06604db82e4ef9e7d4c1c848ce
binsky <timo@binsky.org>
2021-12-29 00:19:50 +0100

    auto disable Telegram api webhook if two-way mode is not configured

--- src/Command/TelegramProcessUpdatesCommand.php
+++ src/Command/TelegramProcessUpdatesCommand.php
@@ -34,13 +34,35 @@ class TelegramProcessUpdatesCommand extends Command {
      * @param ConsoleIo $io
      */
     public function execute(Arguments $args, ConsoleIo $io) {
+        echo '-------------------------------------------------------------------------------' . PHP_EOL;
+
         if ($args->hasOption('api-token') && $args->getOption('api-token') != '') {
             $token = $args->getOption('api-token');
         }
 
         $TelegramActions = new TelegramActions(isset($token) && $token !== "" ? $token : null);
         if (!$TelegramActions->isTwoWayWebhookEnabled()) {
-            $TelegramActions->processUpdates($TelegramActions->getUpdates());
+            $updates = $TelegramActions->getUpdates();
+            if ($updates instanceof \Exception) {
+                if ($updates instanceof \TelegramBot\Api\Exception && $updates->getCode() == 409) {
+                    // Conflict: can't use getUpdates method while webhook is active; use deleteWebhook to delete the webhook first
+                    $TelegramActions->disableWebhook();
+                    echo 'Disable maybe misconfigured Telegram api webhook.' . PHP_EOL;
+                    $updates = $TelegramActions->getUpdates();
+                } else {
+                    echo $updates->getMessage() . PHP_EOL;
+                    exit($updates->getCode());
+                }
+            }
+
+            if (!($updates instanceof \Exception) && sizeof($updates) > 0) {
+                $TelegramActions->processUpdates($updates);
+                echo sizeof($updates) . ' Telegram updates processed.' . PHP_EOL;
+            } else {
+                echo 'No Telegram updates processed.' . PHP_EOL;
+            }
+        } else {
+            echo 'Telegram api requests are disabled because webhooks are configured!' . PHP_EOL;
         }
     }
 }
