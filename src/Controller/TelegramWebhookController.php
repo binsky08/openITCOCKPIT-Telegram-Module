8a478f9ad3b657c9140e7c58587a7533dfc6cd61
binsky <timo@binsky.org>
2024-02-18 00:39:09 +0100

    reformat TelegramWebhookController

--- src/Controller/TelegramWebhookController.php
+++ src/Controller/TelegramWebhookController.php
@@ -4,23 +4,34 @@ declare(strict_types=1);
 
 namespace TelegramModule\Controller;
 
+use itnovum\openITCOCKPIT\Exceptions\HostNotFoundException;
+use itnovum\openITCOCKPIT\Exceptions\ServiceNotFoundException;
+use TelegramBot\Api\Exception;
+use TelegramBot\Api\InvalidArgumentException;
 use TelegramModule\Lib\TelegramActions;
 
 /**
  * Class TelegramWebhookController
  * @package TelegramModule\Controller
  */
-class TelegramWebhookController extends AppController {
-
-    public function notify() {
+class TelegramWebhookController extends AppController
+{
+    /**
+     * @throws ServiceNotFoundException
+     * @throws InvalidArgumentException
+     * @throws Exception
+     * @throws HostNotFoundException
+     */
+    public function notify()
+    {
         $this->set('successful', false);
-        $TelegramActions = new TelegramActions();
+        $telegramActions = new TelegramActions();
 
-        if ($TelegramActions->isTwoWayWebhookEnabled()) {
-            $update = $TelegramActions->parseUpdate($this->request->getData());
+        if ($telegramActions->isTwoWayWebhookEnabled()) {
+            $update = $telegramActions->parseUpdate($this->request->getData());
 
             if ($update !== false) {
-                $TelegramActions->processUpdate($update);
+                $telegramActions->processUpdate($update);
                 $this->set('successful', true);
             }
         }
